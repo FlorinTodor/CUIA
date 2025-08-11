9fd22ff7b913016719031afc34582bd9314b0ac5
Florín Emanuel Todor Gliga <76227075+FlorinTodor@users.noreply.github.com>
2025-08-11 12:41:08 +0200

    Delete lib/python3.12/site-packages directory

--- lib/python3.12/site-packages/pygfx/__pyinstaller/hook-pygfx.py
+++
@@ -1,8 +0,0 @@
-# ruff: noqa
-
-from PyInstaller.utils.hooks import collect_data_files
-
-hiddenimports = ["pygfx.data_files"]
-datas = []
-datas += collect_data_files("pygfx.data_files")
-datas += collect_data_files("pygfx.renderers.wgpu.wgsl")
--- lib/python3.12/site-packages/pygfx/renderers/svg/__init__.py
+++
@@ -1,4 +0,0 @@
-# ruff: noqa: F401
-
-from ._svgrenderer import SvgRenderer, registry, register_svg_render_function
-from . import linerender
